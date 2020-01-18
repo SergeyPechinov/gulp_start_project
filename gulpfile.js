5428d92b0241e178059da38902518bd2739d18b8
Sergey Pechinov <sen1c163rus@gmail.com>
2020-01-18 16:35:20 +0400

    start gulp project

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var gulp = require('gulp'),
+		sass = require('gulp-sass'),
+		pug = require('gulp-pug'),
+		concatCss = require('gulp-concat-css'),
+		browserSync = require('browser-sync').create();
+
+gulp.task('server', function() {
+	browserSync.init({
+		server: {
+			baseDir: './build',
+		}
+	});
+	browserSync.watch('build', browserSync.reload);
+});
+
+gulp.task('pug', function () {
+	return gulp.src('src/pug/**/*.pug')
+			.pipe(pug({
+				pretty: true,
+			}))
+			.pipe(gulp.dest('build'));
+});
+
+gulp.task('sass', function () {
+	return gulp.src('src/scss/**/*.scss')
+			.pipe(sass())
+			.pipe(gulp.dest('src/css'))
+});
+
+gulp.task('images', function () {
+	return gulp.src('src/images/**/*').pipe(guld.dest('build/images'));
+});
+
+gulp.task('concatCss', function () {
+	return gulp.src('src/css/**/*')
+			.pipe(concatCss('css/css.css'))
+			.pipe(gulp.dest('build'));
+});
+
+gulp.task('watch', function () {
+	gulp.series('pug');
+	gulp.series('sass');
+	gulp.series('images');
+	gulp.series('concatCss');
+
+	gulp.watch('src/pug/**/*.pug', gulp.series('pug'));
+	gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
+	gulp.watch('src/images/**/*', gulp.series('images'));
+	gulp.watch('src/css/**/*', gulp.series('concatCss'));
+});
+
+gulp.task('default', gulp.series(
+	gulp.parallel('watch', 'server')
+));
